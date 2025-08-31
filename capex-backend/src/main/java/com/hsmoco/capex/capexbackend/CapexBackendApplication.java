de471dd7de29930bef1cc551b1b7b2e37a39671c
Henrique Smoco <henriquesmoco@outlook.com>
2025-08-31 00:20:25 -0500

    Created backend module

---
+++ capex-backend/src/main/java/com/hsmoco/capex/capexbackend/CapexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hsmoco.capex.capexbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapexBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapexBackendApplication.class, args);
+	}
+
+}
---
+++ capex-backend/src/test/java/com/hsmoco/capex/capexbackend/CapexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsmoco.capex.capexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
